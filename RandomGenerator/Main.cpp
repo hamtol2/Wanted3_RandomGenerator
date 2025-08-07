628334b6a299d299f379fe11d28c1fb559b955ff
ronniej <ronniej@naver.com>
2025-08-07 12:30:08 +0900

    Wanted3기 발표 순서를 정하기 위한 랜덤 생성기

---
+++ RandomGenerator/Main.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+
+void RandomGenerator(const char* filename, int drawCount = 5);
+
+int main(int argc, char* argv[])
+{
+	if (argc == 0)
+	{
+		std::cout << "추첨할 파일 이름을 넣어주세요.\n";
+		std::cout << "usage1: RandomGenerator.exe filename\n";
+		std::cout << "usage2: RandomGenerator.exe filename drawCount\n";
+		return -1;
+	}
+
+	const char* filename = "PresentationList.txt";
+	int drawCount = 5;
+	if (argc == 2)
+	{
+		filename = argv[1];
+	}
+	else if (argc == 3)
+	{
+		filename = argv[1];
+		drawCount = atoi(argv[2]);
+	}
+
+	RandomGenerator(filename, drawCount);
+}
+
+void RandomGenerator(const char* filename, int drawCount)
+{
+	std::cout << filename << ", " << drawCount << "\n";
+
+	char finalPath[MAX_PATH] = {};
+	sprintf_s(finalPath, MAX_PATH - 1, "../TextList/%s", filename);
+
+	FILE* file = nullptr;
+	fopen_s(&file, finalPath, "rt");
+	if (file == nullptr)
+	{
+		__debugbreak();
+		int errorCode = GetLastError();
+		char debugMessage[100] = {};
+		sprintf_s(debugMessage, 100, "Failed to open file. errorCode: %d\n", errorCode);
+		//OutputDebugStringA(debugMessage);
+		std::cout << debugMessage;
+		return;
+	}
+
+	fseek(file, 0, SEEK_END);
+	size_t fileSize = ftell(file);
+	rewind(file);
+
+	//char* buffer = new char[fileSize] + 1;
+	char buffer[2048];
+	memset(buffer, 0, sizeof(char) * 2048);
+
+	size_t readSize = fread(buffer, sizeof(char), 2048, file);
+
+	char* context = nullptr;
+	char* line = strtok_s(buffer, "\n", &context);
+	std::vector<const char*> allNames;
+
+	while (line)
+	{
+		allNames.emplace_back(line);
+		line = strtok_s(nullptr, "\n", &context);
+	}
+
+	// 이름순 정렬 후 출력.
+	for (int ix = 0; ix < (int)allNames.size(); ++ix)
+	{
+		for (int jx = 0; jx < (int)allNames.size() - 1; ++jx)
+		{
+			if (strcmp(allNames[jx], allNames[jx + 1]) > 0)
+			{
+				std::swap(allNames[jx], allNames[jx + 1]);
+				continue;
+			}
+		}
+	}
+
+	// 무작위 추첨 (drawCount 번).
+	std::vector<int> tempVector;
+	std::vector<int> reserveVector;
+	std::vector<const char*> presentationNames;
+	tempVector.reserve(allNames.size());
+	reserveVector.reserve(allNames.size());
+	presentationNames.reserve(allNames.size());
+
+	// 발표자 인덱스 임시 배열에 넣기.
+	for (int ix = 0; ix < (int)allNames.size(); ++ix)
+	{
+		tempVector.emplace_back(ix);
+	}
+
+	srand((int)time(nullptr));
+
+	// drawCount만큼 무작위 추첨 시작.
+	for (int count = 0; count < drawCount; ++count)
+	{
+		// 0-23번 번호를 가진 배열로 시작.
+		for (int ix = 0; (int)tempVector.size(); ++ix)
+		{
+			// 0-23 인덱스를 하나씩 뽑으면서 랜덤 진행.
+			int diff = ((int)tempVector.size() - 1);
+			int index = (diff * rand()) / (RAND_MAX + 1);
+			reserveVector.emplace_back(tempVector[index]);
+
+			tempVector.erase(tempVector.begin() + index);
+		}
+
+		// drawCount번 더 진행.
+		tempVector.swap(reserveVector);
+	}
+
+	// 무작위 추첨한 순서로 이름 배열 생성.
+	for (int ix = 0; ix < (int)tempVector.size(); ++ix)
+	{
+		presentationNames.emplace_back(allNames[tempVector[ix]]);
+	}
+
+	std::cout << "\n";
+
+	for (const char* name : presentationNames)
+	{
+		std::cout << name << "\n";
+	}
+
+	fclose(file);
+}
